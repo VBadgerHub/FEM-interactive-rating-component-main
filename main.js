a41a68abb9ff5bc19c803f4c2e7d2313f3dfa112
Vorczu <vorczu89@gmail.com>
2022-08-05 22:41:18 +0200

    Rating window ready

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+let lastId = ''
+
+const ratCilcked = (id) => {
+    document.getElementById(id).classList.add('n_active')
+    lastId ? document.getElementById(lastId).classList.remove('n_active') : null
+    lastId = id
+}
+
+const addRating = () => {
+    document.getElementById('rating_container').classList.add('rating_box_fade')
+    document.getElementById('thanks_container').classList.add('thanks_container_fade')
+}
+
+
+
